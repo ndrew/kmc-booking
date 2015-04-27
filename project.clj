068e185e34e5a785617cfb9fdd0b4575e264deb9
Andrew Sernyak <ndrew.sernyak@gmail.com>
2015-04-27 20:27:06 +0300

    starting with just ring

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject kmc-booking "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/java.jdbc "0.3.2"]
+                 [postgresql "9.1-901.jdbc4"]
+                 [ring/ring-jetty-adapter "1.2.1"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.4"]]
+  :main ^:skip-aot kmc-booking.web
+  :uberjar-name "kmc-booking-standalone.jar"
+  :profiles {:uberjar {:aot :all}})
---
+++ src/kmc_booking/web.clj
@@ -0,0 +1,24 @@
+(ns kmc-booking.web
+	(:require [compojure.core :refer [defroutes]]
+	          [ring.adapter.jetty :as ring]
+	          [compojure.route :as route]
+	          [compojure.handler :as handler])
+	(:gen-class))
+
+
+(defroutes routes
+  ;;shouts/routes
+  (route/resources "/")
+  ;(route/not-found (layout/four-oh-four))
+  )
+
+(def application (handler/site routes))
+
+(defn start [port]
+  (ring/run-jetty application {:port port
+                               :join? false}))
+
+(defn -main []
+  ;(schema/migrate)
+  (let [port (Integer. (or (System/getenv "PORT") "8080"))]
+    (start port)))
