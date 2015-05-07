10b70d118f4cbaffa13cde32f379db1f68f72a70
Andrew Sernyak <ndrew.sernyak@gmail.com>
2015-05-07 13:10:52 +0300

    proper seats layout

--- src-cljs/kmc_booking/core.cljs
+++ src-cljs/kmc_booking/core.cljs
@@ -48,53 +48,113 @@
 ;; (rum/defc name doc-string? [< mixins+]? [params*] render-body+)
 
 (rum/defc seat-plan < rum/cursored-watch [app-state]
-	(let [seats (rum/cursor app-state [:seats])]
+	(let [seats (rum/cursor app-state [:seats])
+
+		  left_side_cols (reverse (range 37 40))
+		  side_rows (range 1 12)
+		  
+		  left_side_house (conj 
+							(into [:div] (map #(vector :div.col_num [:div %]) left_side_cols))
+							(map (fn[[y x]] (vector :div.seat {:x x :y y} "")) 
+								(for [y side_rows x left_side_cols] [y x]))
+							(into [:div] (map #(vector :div.col_num [:div %]) left_side_cols))
+							)
+
+		  right_side_cols (reverse (range 1 4))
+		  right_side_house (conj 
+							(into [:div] (map #(vector :div.col_num [:div %]) right_side_cols))
+							(map (fn[[y x]] (vector :div.seat {:x x :y y} "")) 
+								(for [y side_rows x right_side_cols] [y x]))
+							(into [:div] (map #(vector :div.col_num [:div %]) right_side_cols))
+							)
+
+		  house_rows (range 1 14)
+
+		  left_house_cols (reverse (range 20 37))
+		  left_house (conj 
+							(into [:div] (map #(vector :div.col_num [:div %]) left_house_cols)) 
+							(map (fn[[y x]] (vector :div.seat {:x x :y y} "")) 
+								(for [y house_rows x left_house_cols] [y x]))
+							(into [:div] (map #(vector :div.col_num [:div %]) left_house_cols)) 
+
+					 )
+
+		  right_house_cols (reverse (range 4 20)) 							
+	      right_house (conj 
+							(into [:div] (map #(vector :div.col_num [:div %]) right_house_cols)) 
+							(map (fn[[y x]] (vector :div.seat {:x x :y y} "")) 
+								(for [y house_rows x right_house_cols] [y x]))
+							(into [:div] (map #(vector :div.col_num [:div %]) right_house_cols)))
+
+	      back_house_rows (range 14 20)
+
+	      beletage_cols (range 1 32)
+	      beletage (conj 
+							(into [:div] (map #(vector :div.col_num [:div %]) beletage_cols)) 
+							(map (fn[[y x]] (vector :div.seat {:x x :y y} "")) 
+								(for [y back_house_rows x beletage_cols] [y x]))
+							(into [:div] (map #(vector :div.col_num [:div %]) beletage_cols)))
+
+	      beletage_last_cols (range 1 26)
+	      beletage_last_rows (range 21 22)
+	      
+	      beletage_last_row  (conj [:div#beletage_last_row]
+									(map (fn[[y x]] (vector :div.seat {:x x :y y} "")) 
+										(for [y beletage_last_rows x beletage_last_cols] [y x]))
+	      						(into [:div] (map #(vector :div.col_num [:div %]) beletage_last_cols))
+	      						)
+	      						 
+									
+									
+
+		]
+
+
+
 		[:div.seat-plan 
 			[:div#parter 
-				
 				(into 
 					[:div#rows.left]
 					 	[[:div.col_num [:div {:__html "&nbsp;"}]]
-					 	[:div.col_num [:div {:__html "&nbsp;"}]]
-						(map #(vector :div.row_num %) (range 1 14))])
+					 	(map #(vector :div.row_num [:div %]) (range 1 14))])
 
 				(into [:div#rows.right]
 					 	[[:div.col_num [:div {:__html "&nbsp;"}]]
-					 	[:div.col_num [:div {:__html "&nbsp;"}]]
-						(map #(vector :div.row_num %) (range 1 14))])
+					 	(map #(vector :div.row_num [:div %]) (range 1 14))])
 
-				[:div#left_side_house.side_house 
-					(let [xs (reverse (range 37 40))
-					      ys (range 1 14)]
-						(concat 
-							(into [:div] (map #(vector :div.col_num %) xs))
-							(map (fn[[y x]] (vector :div.seat {:x x :y y} (pr-str [x y]))) 
-								(for [y ys x xs] [y x])
-								)
-							)
-						)
-				]
-				[:div#left_house.house 
-					(into [:div] (map #(vector :div.col_num %) (reverse (range 20 37))))
-					]
-				[:div#right_house.house 
-					(into [:div] (map #(vector :div.col_num %) (reverse (range 4 20))))
+				
+				[:div#left_side_house.side_house left_side_house]
 
-					]
-				[:div#right_side_house.side_house
-					(into [:div] (map #(vector :div.col_num %) (reverse (range 1 4))))
-
-					]
+				[:div#left_house.house   left_house]
+				[:div#right_house.house  right_house]
+				[:div#right_side_house.side_house right_side_house]
 
 				]
 
 			[:div#back_house 
-				[:div#rows.left "1 2 3 "]
-				[:div#rows.right "1 2 3 "]
-				[:div#beletage "!"]
-				[:div#beletage_last_row ""]
+				(into 
+
+					(into 
+						[:div#rows.left]
+						 	[[:div.col_num [:div {:__html "&nbsp;"}]]
+						 	(map #(vector :div.row_num [:div %]) (range 14 21))
+						 	])
+					[[:div.col_num [:div {:__html "&nbsp;"}]]
+						[:div.row_num [:div "21"]]])
+
+				(into 
+					(into 
+						[:div#rows.right]
+						 	[[:div.col_num [:div {:__html "&nbsp;"}]]
+						 	(map #(vector :div.row_num [:div %]) (range 14 21))])
+					[[:div.col_num [:div {:__html "&nbsp;"}]]
+						[:div.row_num [:div "21"]]])
+
+
+				[:div#beletage beletage]
+				beletage_last_row 
 			]
-			(str (count @seats))
+			;(str (count @seats))
 			]
 		)
 )
