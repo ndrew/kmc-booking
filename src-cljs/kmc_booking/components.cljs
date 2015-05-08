0a0bffab239b1990c61927371b70a4d04f5cd597
Andrew Sernyak <ndrew.sernyak@gmail.com>
2015-05-08 18:03:00 +0300

    WIP admin part

--- src-cljs/kmc_booking/components.cljs
+++ src-cljs/kmc_booking/components.cljs
@@ -3,6 +3,8 @@
   			[rum :as rum]
   			[clojure.string :as string]
   			[kmc-booking.util :refer [ajax]]
+
+  			[goog.i18n.DateTimeFormat :as dtf]
   			))
 
 ;;;;;
@@ -37,6 +39,30 @@
   			"paid" "paid"
   			"pending" "pending"
 			)))
+
+(def format-map
+  (let [f goog.i18n.DateTimeFormat.Format]
+{:FULL_DATE (.-FULL_DATE f)
+:FULL_DATETIME (.-FULL_DATETIME f)
+:FULL_TIME (.-FULL_TIME f)
+:LONG_DATE (.-LONG_DATE f)
+:LONG_DATETIME (.-LONG_DATETIME f)
+:LONG_TIME (.-LONG_TIME f)
+:MEDIUM_DATE (.-MEDIUM_DATE f)
+:MEDIUM_DATETIME (.-MEDIUM_DATETIME f)
+:MEDIUM_TIME (.-MEDIUM_TIME f)
+:SHORT_DATE (.-SHORT_DATE f)
+:SHORT_DATETIME (.-SHORT_DATETIME f)
+ :SHORT_TIME (.-SHORT_TIME f)}))
+
+
+(defn format-date-generic
+  "Format a date using either the built-in goog.i18n.DateTimeFormat.Format enum
+or a formatting string like \"dd MMMM yyyy\""
+  [date-format date]
+  (.format (goog.i18n.DateTimeFormat.
+            (or (date-format format-map) date-format))
+           (js/Date. date)))
 
 ;;;;;
 ;
@@ -301,9 +327,13 @@
   		 date :date
   		 } booking]
 
-  [:div 
-
-  	[:span {:style {:width "400px" :display "inline-block"}} (str name " (" phone ") ")] 
+  [:div.booking-line
+  	;[:pre (pr-str booking)]
+  	[:span
+  		[:span.date (format-date-generic :SHORT_DATETIME date)]
+  		[:span.name name]
+  		[:span.phone phone] 
+  	] 
   	
   	[:button "підтвердити"]
   	[:button "скасувати"]
